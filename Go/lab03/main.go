010f78dca4ac6bfc20d82c4f21d614cd269f72ab
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-03-09 20:50:45 +0100

    collatz-lab03

--- Go/lab03/main.go
+++ Go/lab03/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/piotrd22/studia-archiwum/Go/lab03/collatz"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	// flag.Parse()
+	// n, _ := strconv.ParseFloat(flag.Arg(0), 64)
+	// collatz.Collatz(n)
+	// collatz.Collatzv2(n, 0)
+	// collatz.Collatz(n)
+	collatz.FindLongest(4000, 5000)
 }
