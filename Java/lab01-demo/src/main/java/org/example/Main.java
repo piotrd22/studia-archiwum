d45b2397ffda27bd09e9bde129a2f392c390eea6
Piotr Damrych <piotr.damrych2002@gmail.com>
2023-03-09 19:10:18 +0100

    stack-class and tests

--- Java/lab01-demo/src/main/java/org/example/Main.java
+++ Java/lab01-demo/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.stack.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack stack = new Stack();
+
+        stack.push(1);
+        stack.push(10);
+        stack.push(23);
+        stack.push(34);
+        stack.push(-3);
+        stack.push(6);
+        stack.push(7);
+        stack.push(9);
+        stack.push(12);
+        stack.push(4);
+        stack.push(12);
+
+        stack.printStack();
+        System.out.println(stack.getSize());
+
+        stack.pop();
+
+        stack.printStack();
+        System.out.println(stack.getSize());
     }
 }
