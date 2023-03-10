cb8679fae232341bf971ade321dfce6950094da8
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-03-10 12:19:42 +0100

    added peek

--- Java/lab01-demo/src/test/java/org/example/stack/tests/TestStack.java
+++ Java/lab01-demo/src/test/java/org/example/stack/tests/TestStack.java
@@ -4,6 +4,7 @@ import org.example.stack.Stack;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class TestStack {
     private Stack stack;
 
@@ -47,5 +48,22 @@ public class TestStack {
 
         assertEquals("pop", 119, result);
         assertEquals("pop", 119, size);
+    }
+
+    @Test
+    public void peek() {
+        for (int i = 0; i < 120; i++) {
+            this.stack.push(i);
+        }
+        int result = this.stack.peek();
+        int size = this.stack.getSize();
+
+        assertEquals("pop", 119, result);
+        assertEquals("pop", 120, size);
+    }
+
+    @Test
+    public void peekFailure() {
+        assertThrows(IllegalArgumentException.class, () -> this.stack.peek());
     }
 }
