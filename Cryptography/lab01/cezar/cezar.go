807aa309cc586a5d0a53de0cff0afca5cd319e14
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-03-12 19:05:41 +0100

    added decrypt

--- Cryptography/lab01/cezar/cezar.go
+++ Cryptography/lab01/cezar/cezar.go
@@ -25,7 +25,7 @@ func EncryptMessage() string {
 
 	key = key % 26
 
-	message_with_no_spaces := strings.ReplaceAll(message, " ", "")
+	message_with_no_spaces := strings.ReplaceAll(message, " ", "\n")
 	msg := []rune(message_with_no_spaces)
 
 	for i := 0; i < len(msg); i++ {
@@ -46,7 +46,53 @@ func EncryptMessage() string {
 		crypto += string(rune(intMsg))
 	}
 
-	files.WriteToFile("data/crypto.txt", crypto)
+	crypto_to_save := strings.ReplaceAll(crypto, "\n", " ")
+	files.WriteToFile("data/crypto.txt", crypto_to_save)
 
-	return crypto
+	return crypto_to_save
+}
+
+func DecryptMessage() string {
+	message := ""
+
+	cryptogram := files.ReadFromFile("data/crypto.txt")
+	crypto := strings.ReplaceAll(cryptogram, " ", "\n")
+
+	key_string := files.ReadFromFile("data/key.txt")
+
+	key, err := strconv.Atoi(key_string)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if key < 0 || key > 25 {
+		log.Fatal("Wrong key!")
+	}
+
+	key = key % 26
+
+	msg := []rune(crypto)
+
+	for i := 0; i < len(msg); i++ {
+		intMsg := int(msg[i])
+		if intMsg > 64 && intMsg < 91 {
+			intMsg -= key
+			if intMsg < 65 {
+				intMsg += 26
+			}
+		}
+
+		if intMsg > 96 && intMsg < 123 {
+			intMsg -= key
+			if intMsg < 97 {
+				intMsg += 26
+			}
+		}
+		message += string(rune(intMsg))
+	}
+
+	message_to_save := strings.ReplaceAll(message, "\n", " ")
+	files.WriteToFile("data/decrypt.txt", message_to_save)
+
+	return message_to_save
 }
