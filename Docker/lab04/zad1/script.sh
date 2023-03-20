bbc37d921b46aa50e156c96a62479edc9736e86a
Piotr Damrych <piotr.damrych2002@gmail.com>
2023-03-20 17:41:32 +0100

    lab04zad1

---
+++ Docker/lab04/zad1/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker volume create nginx_data
+
+docker run -d \
+  --name lab01zad1 \
+  -p 80:80 \
+  -v nginx_data:/usr/share/nginx/html \
+  nginx
+
+echo "Hello World!" > \\\\wsl$\\docker-desktop-data\\data\\docker\\volumes\\nginx_data\\_data\\index.html
+
+if [[ $(curl -s localhost) == *"Hello World!"* ]]; then
+  echo "Nginx server is working correctly."
+else
+  echo "Nginx server is not working correctly."
+fi
+
+echo "Changes" > \\\\wsl$\\docker-desktop-data\\data\\docker\\volumes\\nginx_data\\_data\\index.html
+
+if [[ $(curl -s localhost) == *"Changes"* ]]; then
+  echo "Nginx server is working correctly."
+else
+  echo "Nginx server is not working correctly."
+fi
