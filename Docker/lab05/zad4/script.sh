e411ebb879b99ddd05b6586f3eeb3ed8644ea70f
Piotr Damrych <piotr.damrych2002@gmail.com>
2023-03-27 17:47:31 +0200

    lab05

--- Docker/lab05/zad4/script.sh
+++ Docker/lab05/zad4/script.sh
@@ -2,4 +2,4 @@
 
 docker build . -t lab05zad4
 
-docker run -it -p 5000:5000 -v C:/Users/Piotr Damrych/Desktop/UCZELNIA/studia-archiwum/Docker/lab05/zad4/myapp lab05zad4
+docker run -it -p 5000:5000 -v C:/Users/"Piotr Damrych"/Desktop/UCZELNIA/studia-archiwum/Docker/lab05/zad4/myapp lab05zad4
