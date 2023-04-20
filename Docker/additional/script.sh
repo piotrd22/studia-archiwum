3f145c3743c1bb91fb74d039d1029824fa4469b0
Piotr Damrych <piotr.damrych2002@gmail.com>
2023-04-20 11:15:46 +0200

    some work

--- Docker/additional/script.sh
+++ Docker/additional/script.sh
@@ -2,4 +2,10 @@
 
 # we dont need to use nginx.conf bc default is good
 
-docker compose up
+# docker compose up
+
+docker create app_network
+
+docker build -t app --build-arg GIT_REPO https://github.com/szymon-kalkowski/meme-generator.git .
+
+docker run -d --network app_network -p 80:80 --name app app
